71a0109c5f89a28ddc39f66d5946fa3f95dd531b
Sebastien Francois <sf2@ecs.soton.ac.uk>
2014-07-04 11:10:11 +0100

    also cache 'views' aka summary page hits

--- cfg/plugins/EPrints/Plugin/Stats/Processor/EPrint/CacheDownloads.pm
+++ cfg/plugins/EPrints/Plugin/Stats/Processor/EPrint/CacheDownloads.pm
@@ -16,11 +16,19 @@ sub new
         my( $class, %params ) = @_;
 	my $self = $class->SUPER::new( %params );
 
-	$self->{provides} = [ "cache_downloads" ];
+	$self->{provides} = [ "cache_downloads", "cache_views" ];
 
 	$self->{disable} = 0;
 
 	return $self;
+}
+
+sub clear_cache
+{
+	my( $self ) = @_;
+
+	delete $self->{cache_downloads};
+	delete $self->{cache_views};
 }
 
 sub process_record
@@ -30,15 +38,24 @@ sub process_record
 	my $epid = $eprint->get_id;
 	return unless( defined $epid );
 	
-	my $ctx = $self->{handler}->context( { datatype => "downloads", set_name => 'eprint', set_value => $epid } );
+	foreach my $type ( "downloads", "views" )
+	{
+		my $ctx = $self->{handler}->context( { datatype => $type, set_name => 'eprint', set_value => $epid, cache => 0 } );
 
-	my $count = $self->{handler}->data( $ctx )->select->sum_all;
+		my $count = $self->{handler}->data( $ctx )->select->sum_all;
 
-	my $datestamp = "0";
-
-	# "0" is a place-holder for the datestamp which is not used in the downloads_cache table (that's the point)
-	$self->{cache}->{"0"}->{$epid}->{"downloads"} = $count;
+		# "0" is a place-holder for the datestamp which is not used in the downloads_cache table (that's the point)
+		$self->{"cache_$type"}->{"0"}->{$epid}->{"type"} = $count;
+	}
 }
 
+# use this to commit to db:
+sub commit_data
+{
+	my( $self, $handler ) = @_;
+
+	$handler->save_data_values( "cache_downloads", $self->{cache_downloads} );
+	$handler->save_data_values( "cache_views", $self->{cache_views} );
+}
 
 1;
