ef421e043bd80fb8ed03f943a372a15e6f0de487
David R Newman <drn@ecs.soton.ac.uk>
2024-01-18 15:36:39 +0000

    Avoids loading Google JS unless a page that displays a chart

--- cfg/cfg.d/z_irstats2.pl
+++ cfg/cfg.d/z_irstats2.pl
@@ -277,6 +277,23 @@ $c->add_trigger( $EPrints::Plugin::Stats::EP_TRIGGER_DYNAMIC_TEMPLATE, sub
 
         my( $repo, $pins ) = @args{qw/ repository pins/};
 
+	# Only include Google Charts APIs if needed on current page. 
+	my $stats_path = '^' . $repo->config( "rel_cgipath" ) . "/stats/";
+	my $abstract_path = '^' . $repo->config( "rel_path" ) . '/[0-9]+/?$';
+	my $abstract_long_path = '^' . $repo->config( "rel_path" ) . '/id/eprint/[0-9]+/?';
+	my $for_stats = 0;
+	# IRStats2 pages
+	if ( $repo->get_request->uri =~ m!$stats_path! )
+	{
+		$for_stats = 1;
+	}
+	# Abstract pages if IRStats2 box plugin enabled
+	elsif ( $repo->config( 'plugins', 'Screen::EPrint::Box::Stats', 'params', 'disable' ) == '0' && ( $repo->get_request->uri =~ m!$abstract_path! || $repo->get_request->uri =~ m!$abstract_long_path! ) )
+	{
+		$for_stats = 1;
+	}
+	return EP_TRIGGER_OK unless $for_stats;
+		
         my $head = $repo->make_doc_fragment;
 
         $head->appendChild( $repo->make_javascript( undef,
@@ -658,8 +675,8 @@ $c->{plugins}{"Stats::View::Table"}{params}{disable} = 0;
 
 $c->{plugins}{"Screen::IRStats2::Report"}{params}{disable} = 0;
 
-# Display download stats for an EPrints on it's summary page?
-# Confusingly, set this to '0' to make them appear, or 1 to not show them
+# Display download stats for an eprint on it's summary page?
+# Confusingly, set this to 0 to make them appear, or 1 to not show them.
 
 $c->{plugins}{"Screen::EPrint::Box::Stats"}{params}{disable} = 1;
 
