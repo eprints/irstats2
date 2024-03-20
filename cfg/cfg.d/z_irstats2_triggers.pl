a7df266a4268777dbada4751d98e0b8e36b5c78f
David R Newman <drn@ecs.soton.ac.uk>
2024-03-20 09:52:15 +0000

    Assume not for stats if request not set

--- cfg/cfg.d/z_irstats2_triggers.pl
+++ cfg/cfg.d/z_irstats2_triggers.pl
@@ -5,18 +5,20 @@ $c->add_trigger( EPrints::Const::EP_TRIGGER_DYNAMIC_TEMPLATE, sub
 
 		my( $repo, $pins ) = @args{qw/ repository pins/};
 
+	return EP_TRIGGER_OK unless defined $repo->get_request;
+
 	# Only include Google Charts APIs if needed on current page.
 	my $stats_path = '^' . $repo->config( "rel_cgipath" ) . "/stats/";
 	my $abstract_path = '^' . $repo->config( "rel_path" ) . '/[0-9]+/?$';
 	my $abstract_long_path = '^' . $repo->config( "rel_path" ) . '/id/eprint/[0-9]+/?';
 	my $for_stats = 0;
 	# IRStats2 pages
-	if ( $repo->get_request->uri =~ m!$stats_path! )
+	if ( defined $repo->get_request && $repo->get_request->uri =~ m!$stats_path! )
 	{
 		$for_stats = 1;
 	}
 	# Abstract pages if IRStats2 box plugin enabled
-	elsif ( $repo->config( 'plugins', 'Screen::EPrint::Box::Stats', 'params', 'disable' ) == '0' && ( $repo->get_request->uri =~ m!$abstract_path! || $repo->get_request->uri =~ m!$abstract_long_path! ) )
+	elsif ( $repo->config( 'plugins', 'Screen::EPrint::Box::Stats', 'params', 'disable' ) == '0' && defined $repo->get_request && ( $repo->get_request->uri =~ m!$abstract_path! || $repo->get_request->uri =~ m!$abstract_long_path! ) )
 	{
 		$for_stats = 1;
 	}
