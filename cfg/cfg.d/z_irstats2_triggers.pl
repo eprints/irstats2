370c184de2e851c6d23826257807dbaff0d55008
David R Newman <drn@ecs.soton.ac.uk>
2024-10-22 17:31:32 +0000

    Better checking for whether JS is need for stats box on abstract pages

--- cfg/cfg.d/z_irstats2_triggers.pl
+++ cfg/cfg.d/z_irstats2_triggers.pl
@@ -12,13 +12,14 @@ $c->add_trigger( EPrints::Const::EP_TRIGGER_DYNAMIC_TEMPLATE, sub
 	my $abstract_path = '^' . $repo->config( "rel_path" ) . '/[0-9]+/?$';
 	my $abstract_long_path = '^' . $repo->config( "rel_path" ) . '/id/eprint/[0-9]+/?';
 	my $for_stats = 0;
+	my $box_disabled = $repo->config( 'plugins', 'Screen::EPrint::Box::Stats', 'params', 'disable' ) || 0;
 	# IRStats2 pages
 	if ( defined $repo->get_request && $repo->get_request->uri =~ m!^$stats_path! )
 	{
 		$for_stats = 1;
 	}
 	# Abstract pages if IRStats2 box plugin enabled
-	elsif ( $repo->config( 'plugins', 'Screen::EPrint::Box::Stats', 'params', 'disable' ) == '0' && defined $repo->get_request && ( $repo->get_request->uri =~ m!^$abstract_path! || $repo->get_request->uri =~ m!^$abstract_long_path! ) )
+	elsif ( !$box_disabled && EPrints::Utils::is_set( $repo->config( 'plugins', 'Screen::EPrint::Box::Stats', 'appears' ) ) && defined $repo->get_request && ( $repo->get_request->uri =~ m!^$abstract_path! || $repo->get_request->uri =~ m!^$abstract_long_path! ) )
 	{
 		$for_stats = 1;
 	}
